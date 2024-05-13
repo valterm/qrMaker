fabbd8c8d67f90db3feb605e135e4410272d466d
Martin Valter <valter.martin@hotmail.com>
2024-05-13 16:11:59 +0200

    reorganize layout

--- app.py
+++ app.py
@@ -8,11 +8,18 @@ from qrcode.image.styledpil import StyledPilImage
 from qrcode.image.styles.moduledrawers.pil import *
 from qrcode.image.styles.colormasks import *
 import tkinter.filedialog 
+import os, sys
+
+def resource_path(relative_path):
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+
+
 
 class QRCodeApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("QrGen")
+        self.root.title("QrMaker")
         self.qr_label = None
         self.timer = None
         self.delay = 0.2
@@ -39,73 +46,65 @@ class QRCodeApp:
 
         # Prettyfy
         # Set app icon to assets/icon.png
-        self.root.iconbitmap("assets/icon.ico")
+        icon_path = resource_path('assets/icon.ico')
+        self.root.iconbitmap(icon_path)
 
         # Layout
-        # Centered Frame for the QR display
-        self.qr_frame = ttk.Frame(self.root, padding=20)
-        self.qr_frame.grid(row=0, column=1, pady=10)
+        self.qr_frame = ttk.Frame(self.root)
+        self.qr_frame.grid(row=0, column=4, padx=10, rowspan=80)
+
+        # sep = ttk.Separator(self.root,orient='vertical')
+        # sep.grid(row=0, column=3, rowspan=400, sticky='ns', padx=10, pady = 30)
 
         # Button to save the qr code
         self.save_button = ttk.Button(self.root, text="Save QR Code", command=self.save_qr_code)
-        self.save_button.grid(row=1, column=1, pady=3, padx=10)
+        self.save_button.grid(row=80, column=4)
 
         # Input field with label "QR Code Text"
-        self.input_label = ttk.Label(self.root, text="QR Code Text", anchor='w')
-        self.input_label.grid(row=2, column=0, pady=3,)
+        self.input_label = ttk.Label(self.root, text="QR Code Text", anchor='s')
+        self.input_label.grid(row=20, column=0, padx=10)
         self.input_field = tk.Text(self.root, width=40, height=10)
-        self.input_field.grid(row=2, column=1, pady=3, padx=10)
+        self.input_field.grid(row=20, column=1, columnspan=2, pady=10)
         self.input_field.bind("<KeyRelease>", self.start_timer)
-
-        # Label "QR Styling", bold text, and padding
-        self.style_label = ttk.Label(self.root, text="QR Styling", font=("TkDefaultFont", 18, "bold"), anchor='w')
-        self.style_label.grid(row=3, column=0, pady=10, padx=20)
 
         # Dropdown for QR style
         self.style_label = ttk.Label(self.root, text="QR Style", anchor='w')
-        self.style_label.grid(row=4, column=0, pady=3, padx=5)
-        self.style_dropdown = ttk.Combobox(self.root, state="readonly", textvariable=self.style_var, values=["default", "rounded", "vertical", "horizontal"])
-        self.style_dropdown.grid(row=4, column=1, pady=3)
+        self.style_label.grid(row=40, column=0)
+        self.style_dropdown = ttk.Combobox(self.root, state="readonly", 
+                                           textvariable=self.style_var, 
+                                           values=["default", "rounded", "vertical", "horizontal"], width=14)
+        self.style_dropdown.grid(row=40, column=1)
         self.style_dropdown.bind("<<ComboboxSelected>>", self.start_timer)
 
-        # Checkbox for rounded eyes
-        self.rounded_label = ttk.Label(self.root, text="Rounded Eyes", anchor='w')
-        self.rounded_label.grid(row=5, column=0, pady=3, padx=5)
-        self.rounded_checkbox = ttk.Checkbutton(self.root, variable=self.rounded_var)
-        self.rounded_checkbox.grid(row=5, column=1, pady=3)
-        self.rounded_checkbox.bind("<Button-1>", self.start_timer)
+        self.transparent_checkbox = ttk.Checkbutton(self.root, text="Rounded Eyes", variable=self.rounded_var)
+        self.transparent_checkbox.grid(row=40, column=2)
+        self.transparent_checkbox.bind("<Button-1>", self.start_timer)
 
         # Spinbox for box size and border size
-        # self.box_size_label = ttk.Label(self.root, text="Box Size", anchor='w')
-        # self.box_size_label.grid(row=6, column=0, pady=3, padx=5)
         self.border_label = ttk.Label(self.root, text="Border Size", anchor='w')
-        self.border_label.grid(row=7, column=0, pady=3, padx=5)
-        # self.box_size_entry = ttk.Spinbox(self.root, width=10, textvariable=self.box_size_var, from_=1, to=50, increment=1, state="NORMAL",command=self.start_timer)
+        self.border_label.grid(row=70, column=0)
         self.border_entry = ttk.Spinbox(self.root, width=10, textvariable=self.border_var, from_=0, to=50, increment=1, state="NORMAL",command=self.start_timer)
-        # self.box_size_entry.grid(row=6, column=1, pady=3)
-        self.border_entry.grid(row=7, column=1, pady=3)
-        # self.box_size_entry.bind("<KeyRelease>", self.start_timer)
-        # self.border_entry.bind("<KeyRelease>", self.start_timer)
+        self.border_entry.grid(row=70, column=1, pady=3)
+        self.border_entry.bind("<KeyRelease>", self.start_timer)
 
         # Add a file selector to select an image to overlay
         self.logo = None
         self.logo = ttk.Label(self.root, text="Overlay Image", anchor='w')
-        self.logo.grid(row=8, column=0, pady=3, padx=5)
-        self.logo = ttk.Button(self.root, text="Select Image", command=self.select_overlay_image)
-        self.logo.grid(row=8, column=1, pady=3)
+        self.logo.grid(row=80, column=0)
+        self.logo = ttk.Button(self.root, text="Select Image", command=self.select_overlay_image, width=10)
+        self.logo.grid(row=80, column=1)
 
         # Checkbox asking if the overlay image should be transparent
-        self.transparent_checkbox = ttk.Checkbutton(self.root, text="Transparent image?", variable=self.overlay_image_is_transparent)
-        self.transparent_checkbox.grid(row=8, column=2, pady=3, padx=10)
+        self.transparent_checkbox = ttk.Checkbutton(self.root, text="Transparent Image", variable=self.overlay_image_is_transparent)
+        self.transparent_checkbox.grid(row=90, column=1, padx=10)
         self.transparent_checkbox.bind("<Button-1>", self.start_timer)
 
         # Add a clear image button
-        self.clear_button = ttk.Button(self.root, text="Clear Image", command=self.clear_image)
-        self.clear_button.grid(row=9, column=1, pady=0,)
+        self.clear_button = ttk.Button(self.root, text="Clear Image", command=self.clear_image, width=10)
+        self.clear_button.grid(row=80, column=2)
 
-        # Add a firendly message to bottom right corner saying "Made with <3 by circo"
-        # self.message = ttk.Label(self.root, text="Made with ♥ by valterm", anchor='e', font=("TkDefaultFont", 6))
-        # self.message.grid(row=10, column=0, pady=10, padx=10)
+        self.empty = ttk.Label(self.root, text="", anchor='w')
+        self.empty.grid(row=100, column=0)
 
         # Display a default QR code initially
         self.update_qr()
@@ -145,14 +144,17 @@ class QRCodeApp:
                 overlay_image = overlay
 
             # Create the qr code image with the embedded image
-            img = qr.make_image(fill="black", back_color="white", image_factory=StyledPilImage, module_drawer=style_map[self.style_var.get()][0], eye_drawer=RoundedModuleDrawer(), embeded_image=overlay_image)
+            img = qr.make_image(fill="black", back_color="white", image_factory=StyledPilImage, 
+                                module_drawer=style_map[self.style_var.get()][0], 
+                                eye_drawer=RoundedModuleDrawer(), embeded_image=overlay_image)
 
             # Cleanup
             overlay.close()
 
-
         else:
-            img = qr.make_image(fill="black", back_color="white", image_factory=StyledPilImage, module_drawer=style_map[self.style_var.get()][0], eye_drawer=RoundedModuleDrawer() if self.rounded_var.get() else SquareModuleDrawer())
+            img = qr.make_image(fill="black", back_color="white", image_factory=StyledPilImage, 
+                                module_drawer=style_map[self.style_var.get()][0], 
+                                eye_drawer=RoundedModuleDrawer() if self.rounded_var.get() else SquareModuleDrawer())
 
         img = img.resize((200, 200), Image.LANCZOS)
         img = ImageTk.PhotoImage(img)
@@ -174,7 +176,6 @@ class QRCodeApp:
 
     def set_properties_and_update_qr(self):
         # self.box_size_var.set(self.box_size_entry.get())
-        print(self.overlay_image_is_transparent.get())
         self.border_var.set(self.border_entry.get())
         self.qr_text.set(self.input_field.get("1.0","end-1c"))
         self.update_qr()
@@ -207,6 +208,7 @@ class QRCodeApp:
 # Main app content
 if __name__ == "__main__":
     root = tk.Tk()
+    root.geometry("+300+300")
+    root.resizable(False, False)
     app = QRCodeApp(root)
     root.mainloop()
-
